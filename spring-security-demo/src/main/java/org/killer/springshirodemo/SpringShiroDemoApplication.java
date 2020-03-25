a96fe4d97b00a31f88bb72e36b86944ab2d527ee
killerWqs2 <killerWqs@gmail.com>
2020-03-25 19:04:11 +0800

    init

--- spring-security-demo/src/main/java/org/killer/springshirodemo/SpringShiroDemoApplication.java
+++ spring-security-demo/src/main/java/org/killer/springshirodemo/SpringShiroDemoApplication.java
@@ -2,9 +2,19 @@ package org.killer.springshirodemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringShiroDemoApplication {
+
+    @GetMapping("/test")
+    public String test() {
+
+        return "hello world!";
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringShiroDemoApplication.class, args);
