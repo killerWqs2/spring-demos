f91e1b4cd376b045f80ebbbf04eee4f2947658d2
killerWqs <1249476676@qq.com>
2020-04-29 22:22:33 +0800

    update demos

---
+++ spring-sharding-jdbc-demo/src/test/java/org/killer/shardingjdbcdemo/ShardingJdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.killer.shardingjdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingJdbcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
